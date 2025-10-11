9629533cd377caca1d72763adf5c58c80f8c2121
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-10-11 11:40:32 -0300

    Base inicial do projeto Nexus

---
+++ src/main/java/com/nexus/nexus_blog/NexusBlogApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.nexus_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexus/nexus_blog/NexusBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus.nexus_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
