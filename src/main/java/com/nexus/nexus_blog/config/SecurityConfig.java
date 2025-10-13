2e2eb767d3e584aed48f0d50a3c2fa7b16cde756
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-10-13 18:35:04 -0300

    feat: Adiciona o SecurityFilterChain para definir regras de acesso

--- src/main/java/com/nexus/nexus_blog/config/SecurityConfig.java
+++ src/main/java/com/nexus/nexus_blog/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.nexus.nexus_blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -15,4 +17,18 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/register", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout.permitAll());
+        return http.build();
+    }
 }
