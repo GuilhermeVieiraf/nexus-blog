ad70cf0150b24bc05b8cf9d4619c14ec2bd09b79
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-10-13 19:29:28 -0300

    feat: Implementa a lógica de processamento do registro de utilizador

--- src/main/java/com/nexus/nexus_blog/controller/AuthController.java
+++ src/main/java/com/nexus/nexus_blog/controller/AuthController.java
@@ -1,17 +1,36 @@
 package com.nexus.nexus_blog.controller;
 
 import com.nexus.nexus_blog.model.User;
+import com.nexus.nexus_blog.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
 
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
         model.addAttribute("user", new User());
         return "auth/register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistration(@Valid @ModelAttribute("user") User user,
+                                      BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "auth/register";
+        }
+        userService.createUser(user);
+        return "redirect:/login";
     }
 
 }
